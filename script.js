1450caa70485f715d177c2f246ef2d62b9157394
divyansh01-git <divyanshatri@gmail.com>
2026-06-12 12:17:10 +0530

    Enter key

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+inputBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
